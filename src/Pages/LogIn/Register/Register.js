891a6b7a13456469e56d535db9ec112edcdf19d4
NRShakib <nrshakib007@gmail.com>
2022-09-11 14:12:11 +0600

    set environment variable

--- src/Pages/LogIn/Register/Register.js
+++ src/Pages/LogIn/Register/Register.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import './Register.css';
 
@@ -11,19 +12,22 @@ const Register = () => {
 
     const handleRegister = event => {
         event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
     }
     return (
         <div className='register-form'>
-            <h2 style={{textAlign: 'center'}}>Please Register</h2>
-            <form>
+            <h2 style={{ textAlign: 'center' }}>Please Register</h2>
+            <form onSubmit={handleRegister}>
                 <input type="text" name="name" id="" placeholder='Your Name' />
                 <input type="email" name="email" id="" placeholder='Your Mail' required />
                 <input type="password" name="password" id="" placeholder='Password' required />
                 <input type="submit" value="Register" />
             </form>
             <p>Already have an account? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogIn}>
-                     Please LogIn
-                </Link></p>
+                Please LogIn
+            </Link></p>
         </div>
     );
 };
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBomGgWPGOKbLmMomAFG1fokK7z7TXEbQY",
-  authDomain: "genius-car-services-5b7b4.firebaseapp.com",
-  projectId: "genius-car-services-5b7b4",
-  storageBucket: "genius-car-services-5b7b4.appspot.com",
-  messagingSenderId: "349351454072",
-  appId: "1:349351454072:web:019436ff971faf64e0948d"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase

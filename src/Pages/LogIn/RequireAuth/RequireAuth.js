ee2d84e76eab40107c64679c3267833e3c3e67e4
NRShakib <nrshakib007@gmail.com>
2022-09-21 22:44:40 +0600

    final

--- src/Pages/LogIn/RequireAuth/RequireAuth.js
+++ src/Pages/LogIn/RequireAuth/RequireAuth.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
 
 const RequireAuth = ({ children }) => {
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
     const [user, loading] = useAuthState(auth);
     const location = useLocation();
 
@@ -14,6 +16,23 @@ const RequireAuth = ({ children }) => {
 
     if (!user) {
         return <Navigate to='/login' state={{ from: location }} replace></Navigate>
+    }
+
+    if (!user.emailVerified) {
+        return <div>
+            <h3 className='text-danger'>Your Email is not Verified</h3>
+            <h5 className='text-success'>Please Verify Email Address</h5>
+            <button
+                className='btn btn-primary'
+                onClick={async () => {
+                    await sendEmailVerification();
+                    toast('Sent email');
+                }}
+            >
+                Verify email
+            </button>
+            <ToastContainer></ToastContainer>
+        </div>
     }
 
     return children;

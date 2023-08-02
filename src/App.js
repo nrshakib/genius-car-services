0f1528d01857e43246e84a35ac2aec861afcdf8b
NRShakib <nrshakib007@gmail.com>
2023-08-02 15:00:14 +0600

    created adding service form using react-hooks-form

--- src/App.js
+++ src/App.js
@@ -1,41 +1,62 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import About from './Pages/About/About';
-import Home from './Pages/Home/Home/Home';
-import Header from './Pages/Shared/Header/Header';
-import Footer from './Pages/Shared/Footer/Footer'
-import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
-import NotFound from './Pages/Shared/NotFound/NotFound';
-import Login from './Pages/LogIn/Login';
-import Register from './Pages/LogIn/Register/Register';
-import CheckOut from './Pages/CheckOut/CheckOut';
-import RequireAuth from './Pages/LogIn/RequireAuth/RequireAuth';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home/Home";
+import Header from "./Pages/Shared/Header/Header";
+import Footer from "./Pages/Shared/Footer/Footer";
+import ServiceDetail from "./Pages/ServiceDetail/ServiceDetail";
+import NotFound from "./Pages/Shared/NotFound/NotFound";
+import Login from "./Pages/LogIn/Login";
+import Register from "./Pages/LogIn/Register/Register";
+import CheckOut from "./Pages/CheckOut/CheckOut";
+import RequireAuth from "./Pages/LogIn/RequireAuth/RequireAuth";
+import AddService from "./Pages/AddService/AddService";
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Routes>
-        <Route path='/' element={
-          <RequireAuth>
-            <Home></Home>
-          </RequireAuth>
-        }></Route>
-        <Route path='/home' element={
-          <RequireAuth>
-            <Home></Home>
-          </RequireAuth>
-        }></Route>
-        <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
-        <Route path='/about' element={<About></About>}></Route>
-        <Route path='/login' element={<Login></Login>}></Route>
-        <Route path='/register' element={<Register></Register>}></Route>
-        <Route path='/checkout' element={
-          <RequireAuth>
-            <CheckOut></CheckOut>
-          </RequireAuth>
-        }></Route>
-        <Route path='/*' element={<NotFound></NotFound>}></Route>
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <Home></Home>
+            </RequireAuth>
+          }
+        ></Route>
+        <Route
+          path="/home"
+          element={
+            <RequireAuth>
+              <Home></Home>
+            </RequireAuth>
+          }
+        ></Route>
+        <Route
+          path="/service/:serviceId"
+          element={<ServiceDetail></ServiceDetail>}
+        ></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route
+          path="/checkout"
+          element={
+            <RequireAuth>
+              <CheckOut></CheckOut>
+            </RequireAuth>
+          }
+        ></Route>
+        <Route
+          path="/addservice"
+          element={
+            <RequireAuth>
+              <AddService />
+            </RequireAuth>
+          }
+        ></Route>
+        <Route path="/*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>

d5873b80b0e3b5516f9dc6c12d6af500da4e0d9a
NRShakib <nrshakib007@gmail.com>
2023-08-03 14:29:07 +0600

    added new functions to navbar

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,46 +1,68 @@
-import { signOut } from 'firebase/auth';
-import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { Link } from 'react-router-dom';
-import auth from '../../../firebase.init';
-import logo from '../../../images/logo.png'
+import { signOut } from "firebase/auth";
+import React from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+import auth from "../../../firebase.init";
+import logo from "../../../images/logo.png";
 
 const Header = () => {
-    const [user] = useAuthState(auth);
+  const [user] = useAuthState(auth);
 
-    const handleSignOut = () => {
-        signOut(auth);
-    }
+  const handleSignOut = () => {
+    signOut(auth);
+  };
 
-    return (
-        <Navbar collapseOnSelect expand="lg" sticky='top' bg="primary" variant="dark">
-            <Container>
-                <Navbar.Brand as={Link} to="home">
-                    <img src={logo} height="30px" alt="" />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="home">Home</Nav.Link>
-                        <Nav.Link href="home#services">Services</Nav.Link>
-                        <Nav.Link href="home#experts">Experts</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Nav.Link as={Link} to="about">About</Nav.Link>
-                        {
-                            user ?
-                                <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>Sign Out</button>
-                                :
-                                <Nav.Link as={Link} to="login">
-                                    Log In
-                                </Nav.Link>
-                        }
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      sticky="top"
+      bg="primary"
+      variant="dark"
+    >
+      <Container>
+        <Navbar.Brand as={Link} to="home">
+          <img src={logo} height="30px" alt="" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="home">Home</Nav.Link>
+            <Nav.Link href="home#services">Services</Nav.Link>
+            <Nav.Link href="home#experts">Experts</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link as={Link} to="about">
+              About
+            </Nav.Link>
+            {user && (
+              <>
+                <Nav.Link as={Link} to="addservice">
+                  Add Service
+                </Nav.Link>
+                <Nav.Link as={Link} to="manageservice">
+                  Manage Service
+                </Nav.Link>
+              </>
+            )}
+            {user ? (
+              <button
+                className="btn btn-link text-white text-decoration-none"
+                onClick={handleSignOut}
+              >
+                Sign Out
+              </button>
+            ) : (
+              <Nav.Link as={Link} to="login">
+                Log In
+              </Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;

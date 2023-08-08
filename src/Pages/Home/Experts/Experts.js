f1251cb762f31ceea012ed8edc9266893e88a33e
NRS\nrsha <nrshakib007@gmail.com>
2023-08-08 23:59:14 +0600

    fetched data using custom hook

--- src/Pages/Home/Experts/Experts.js
+++ src/Pages/Home/Experts/Experts.js
@@ -1,35 +1,32 @@
-import React from 'react';
-import expert1 from '../../../images/experts/expert-1.jpg';
-import expert2 from '../../../images/experts/expert-2.jpg';
-import expert3 from '../../../images/experts/expert-3.jpg';
-import expert4 from '../../../images/experts/expert-4.jpg';
-import expert5 from '../../../images/experts/expert-5.jpg';
-import expert6 from '../../../images/experts/expert-6.png';
-import Expert from '../Expert/Expert';
+import React from "react";
+import expert1 from "../../../images/experts/expert-1.jpg";
+import expert2 from "../../../images/experts/expert-2.jpg";
+import expert3 from "../../../images/experts/expert-3.jpg";
+import expert4 from "../../../images/experts/expert-4.jpg";
+import expert5 from "../../../images/experts/expert-5.jpg";
+import expert6 from "../../../images/experts/expert-6.png";
+import Expert from "../Expert/Expert";
 
 const experts = [
-    { id: 1, name: 'Will', img: expert1 },
-    { id: 2, name: 'Chris', img: expert2 },
-    { id: 3, name: 'Brad', img: expert3 },
-    { id: 4, name: 'Dwayne', img: expert4 },
-    { id: 5, name: 'Rock', img: expert5 },
-    { id: 6, name: 'Vin', img: expert6 },
-]
+  { id: 1, name: "Will", img: expert1 },
+  { id: 2, name: "Chris", img: expert2 },
+  { id: 3, name: "Brad", img: expert3 },
+  { id: 4, name: "Dwayne", img: expert4 },
+  { id: 5, name: "Rock", img: expert5 },
+  { id: 6, name: "Vin", img: expert6 },
+];
 
 const Experts = () => {
-    return (
-        <div className='container'>
-            <h2 className='text-primary text-center mt-5'>Our Experts</h2>
-            <div className="row">
-                {
-                    experts.map(expert => <Expert
-                        key={expert.id}
-                        expert={expert}
-                    ></Expert>)
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div id="experts" className="container">
+      <h2 className="text-primary text-center mt-5">Our Experts</h2>
+      <div className="row">
+        {experts.map((expert) => (
+          <Expert key={expert.id} expert={expert}></Expert>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Experts;
